fe76d388be2303d41057b094c5f1e80322958fce
Ultimate333 <belov.evgenijj333@gmail.com>
2025-07-29 12:00:47 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace c
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            float per = 5;
+            if (per % 2 == 0)
+            {
+                Console.WriteLine("четное");
+            }
+            else
+            {
+                Console.WriteLine("не четное");
+            }
+        }
+    }
+}
+
